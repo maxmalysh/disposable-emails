57fe924b5ba9e7ca27820510d2a3f2578dc55c80
Max Malysh <maxmalysh@users.noreply.github.com>
2017-05-04 23:29:43 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='disposable-emails',
+    version='0.9',
+    description='Weed out disposable email providers with ease',
+    url='https://github.com/maxmalysh/disposable-emails',
+    author='Max Malysh',
+    author_email='hello@maxmalysh.com',
+    license='MIT',
+    packages=['disposable_emails'],
+    zip_safe=False
+)

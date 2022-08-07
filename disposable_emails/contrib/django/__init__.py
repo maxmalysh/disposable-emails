2aa9c4730fc2a87885c9e62fa3ebbbbcb31c9b32
mmalysh <github@maxmalysh.com>
2022-08-08 01:04:19 +0300

    contrib.django: replace deprecated ugettext_lazy to gettext_lazy (adds support for Django 4.0 & 4.1)

--- disposable_emails/contrib/django/__init__.py
+++ disposable_emails/contrib/django/__init__.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from disposable_emails import is_disposable_email, extract_domain
 
 
